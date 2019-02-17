e7da6f9806efb2af8ded9ecce36b600f521c0642
Thanaphat North <thanaphat@ipassion.co.th>
2019-02-17 15:49:37 +0700

    animation

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,9 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, stagger, animate, style, group, query as q, transition, keyframes } from '@angular/animations';
+const query = (s, a, o = { optional: true }) => q(s, a, o);
+
+// trigger('homeTransition',[
+//   transition(':enter',[
+//     query('.home',[
+//       style({transform:'translateX(-500px)'}),
+//       animate('1s cubic-bezier(0.2,0.11,1,0.03)',style({transform:'translateX(0px)'}))
+//     ]),
+//   ])
+// ])
+export const homeTransition = trigger('homeTransition', [
+  transition(':enter', [
+    query('.circle', style({ opacity: 0 })),
+    query('.circle',  [
+      style({ top: '55%' }),
+      animate('1s cubic-bezier(.75,-0.48,.26,1.52)', style({top: '50%', opacity: 1})),
+    ]),
+  ]),
+ 
+]);
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [ homeTransition ],
+  host: {
+    '[@homeTransition]': ''
+  }
+
 })
 export class HomeComponent implements OnInit {
 
--- src/app/page/splash-screen/splash-screen.component.ts
+++ src/app/page/splash-screen/splash-screen.component.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {trigger, stagger, animate, style, group, query as q, transition, keyframes} from '@angular/animations';
+import { HomeComponent } from '../home/home.component';
+const query = (s,a,o={optional:true})=>q(s,a,o);
+
+export const splashTransition = trigger('splashTransition', [
+  transition(':enter', [
+    query('.bg', style({ transform: 'translateX(0px)' })),
+  ]),
+  transition(':leave', [
+    query('.loader',[
+      style({opacity:'1'}),
+      animate('100ms',style({opacity:'0'}))
+    ]),
+    query('.bg', stagger(300, [
+      style({ transform: 'translateX(0px)'}),
+      animate('1s cubic-bezier(0.2,0.11,1,0.03)', style({transform: 'translateX(100%)'})),
+    ])),
+    
+  ])
+]);
+
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.css'],
- 
+   styleUrls: ['./splash-screen.component.css'],
+  animations: [ splashTransition ],
+  host: {
+    '[@splashTransition]': ''
+  }
 })
+
 export class SplashScreenComponent implements OnInit {
 
   constructor(private router: Router) { }
 
   ngOnInit() {
-  //   setInterval(()=>{
-  //     this.router.navigateByUrl('/home');
-
-  //   },2000)
-  // }
+    setInterval(() => {
+      this.router.navigate(['/home']);
+        
+    },2000)
   }
 }
